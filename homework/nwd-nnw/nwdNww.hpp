8c6b2ccc19dc160d70d7e00a71e0170cc2590f8b
Pulsarnixx <marek.mazurczak@protonmail.com>
2026-08-29 17:16:09 +0200

    NWD done.

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    lhs = abs(lhs);
+    rhs = abs(rhs);
+
+    if (rhs == 0) {
+        return lhs;
+    }
+
+    return NWD(rhs, lhs % rhs);
 }
 
 int NWW(int lhs, int rhs) {
