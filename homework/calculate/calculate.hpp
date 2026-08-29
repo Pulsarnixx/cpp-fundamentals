dd6166763fa4499e2e4dfb38ce46587cdde36dad
Pulsarnixx <marek.mazurczak@protonmail.com>
2026-08-29 15:36:25 +0200

    Add done.

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,18 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
-    return "";
+    if(command == "add"){
+        return std::to_string(first + second);
+    }
+    else if(command == "substract"){
+  	//TODO
+    }
+    else if(command == "multiply"){
+     	//TODO
+    }
+    else if(command == "divide"){
+	//TODO
+    }
+
+    return "Invalid data";
 }
