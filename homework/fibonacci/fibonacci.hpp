ab66fd0047c46f73a1d1f5a7a85c5fc53fea1658
Pulsarnixx <marek.mazurczak@protonmail.com>
2026-08-29 16:26:23 +0200

    Fibonacci recursive done.

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -21,6 +21,12 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
+    if (sequence >= 1) {
+        if (sequence == 1) {
+            return 1;
+        }
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
+
     return 0;
 }
