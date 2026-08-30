7e2dfa19deea6f74b9c3198daf51c6d1a880b9df
Pulsarnixx <marek.mazurczak@protonmail.com>
2026-08-30 13:39:40 +0200

    Modified error messages, added checkPasswordRules.

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,45 +1,53 @@
 #include "validation.hpp"
+#include <random>
 
 std::string getErrorMessage(ErrorCode code) {
     switch (code) {
     case ErrorCode::Ok:
         return {
-            "Ok."
-        }
+            "Ok"};
         break;
     case ErrorCode::PasswordNeedsAtLeastNineCharacters:
         return {
-            "Password needs at least nine characters."
-        }
+            "Password needs to have at least nine characters"};
         break;
     case ErrorCode::PasswordNeedsAtLeastOneNumber:
         return {
-            "Password needs at least one number."
-        }
+            "Password needs to have at least one number"};
         break;
     case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
         return {
-            "Password needs at least one special character."
-        }
+            "Password needs to have at least one special character"};
         break;
     case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
         return {
-            "Password needs at least one uppercase letter."
-        }
+            "Password needs to have at least one uppercase letter"};
         break;
     case ErrorCode::PasswordsDoNotMatch:
         return {
-            "Passwords do not match."
-        }
+            "Passwords do not match"};
         break;
     default:
         return {
-            "Invalid error code."
-        }
+            "Invalid error code"};
         break;
     }
 }
 
 bool doPasswordsMatch(const std::string& pass1, const std::string& pass2) {
     return (pass1 == pass2);
+}
+
+ErrorCode checkPasswordRules(const std::string& pass) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, 5);
+
+    const int number = distrib(gen);
+
+    return static_cast<ErrorCode>(number);
+}
+
+ErrorCode checkPassword(const std::string& pass1, const std::string& pass2) {
+    return ErrorCode::Ok;
 }
--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -14,5 +14,8 @@ enum class ErrorCode {
 
 std::string getErrorMessage(ErrorCode code);
 bool doPasswordsMatch(const std::string& pass1, const std::string& pass2);
+ErrorCode checkPasswordRules(const std::string& pass);
+
+ErrorCode checkPassword(const std::string& pass1, const std::string& pass2);
 
 #endif
